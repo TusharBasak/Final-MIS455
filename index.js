fb99c5e9d6c80bf369511bab33d43c1c87426818
TusharBasak <2022315@iub.edu.bd>
2024-04-26 17:45:09 +0600

    more search completed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function show (data) {
    
      
    
-     if(data.length<=5){
+   if(data.length<=5){
        
     for (var i=0; i<5; i++){
    
@@ -33,7 +33,7 @@ function show (data) {
                                   Meal Name: <b>${data[i].strMeal}</b> <br>
                                   <img src="${data[i].strMealThumb}"> <br>
                                   Meal Title: <b>${data[i].strCategory}</b> <br>
-                                  Cooking Instructions: ${data[i].strInstructions}<br><br>`; 
+                                  <b>Cooking Instructions:</b> ${data[i].strInstructions}<br><br>`; 
        oldContent.appendChild(newDiv); 
        
     }
@@ -52,7 +52,7 @@ function show (data) {
                                   Meal Name: <b>${data[i].strMeal}</b> <br>
                                   <img src="${data[i].strMealThumb}"> <br>
                                   Meal Title: <b>${data[i].strCategory}</b> <br>
-                                  Cooking Instructions: ${data[i].strInstructions}<br><br>`; 
+                                  <b>Cooking Instructions: </b>${data[i].strInstructions}<br><br>`; 
            newDiv.classList.add("innerStyle");
            oldContent.appendChild(newDiv); 
            
@@ -64,3 +64,50 @@ function show (data) {
      
    
    }
+
+
+
+   function connect2() {
+  
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`; 
+    oldContent.textContent = ""; 
+    oldContent2.style.display="flex";
+
+    fetch (url)
+    .then (res=> res.json() )
+    .then( data => show2(data.meals))
+}
+
+function show2 (data) {
+    console.log (data); 
+    
+    
+    oldContent2.style.display="none";
+   
+     
+   
+    
+       
+   //  var oldContent2 = document.getElementById("container2");
+    
+    
+   
+       for (var i=5; i<data.length; i++){
+   
+           var newDiv = document.createElement("div");
+           newDiv.innerHTML = `Meal ID: <b>${data[i].idMeal}</b> <br>
+                               Meal Name: <b>${data[i].strMeal}</b> <br>
+                               <img src="${data[i].strMealThumb}"> <br>
+                               Meal Title: <b>${data[i].strCategory}</b> <br>
+                               <b>Cooking Instructions:</b> ${data[i].strInstructions}<br><br>`; 
+           newDiv.classList.add("innerStyle");
+           oldContent.appendChild(newDiv); 
+           
+           
+        
+   
+     }
+
+     
+    }
+    
